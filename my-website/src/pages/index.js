af8579c045f1e4ace65bc8de766cdff1176c0ba2
Bread <86811307+Breadlicia@users.noreply.github.com>
2023-09-17 14:29:52 +0000

    attmepting to make homepage work

--- my-website/src/pages/index.js
+++ my-website/src/pages/index.js
@@ -3,6 +3,14 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import DocPage from '@theme/DocPage'; // Import the DocPage component
+
+export default function Home() {
+  return (
+    <Layout>
+      <DocPage content="/docs/landing.md" />
+    </Layout>
+  );
+}
 
 import styles from './index.module.css';
